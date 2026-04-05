0fcb6c2cbefedec2130c1b8658725a0c130404cd
Afzalbek <gofurovafzalbek7@gmail.com>
2026-04-05 13:49:32 +0500

    First commit

---
+++ src/App.jsx
@@ -0,0 +1,10 @@
+import { Button } from '@/components/ui/button'
+import React from 'react'
+
+function App() {
+  return (
+    <div><Button variant='outline' >Hello World</Button> </div>
+  )
+}
+
+export default App
